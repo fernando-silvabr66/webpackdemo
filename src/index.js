277c28eb6949986f6d1ded37ff25d862514312a1
Fernando Silva <77974883+fernando-silvabr66@users.noreply.github.com>
2022-02-17 19:08:07 -0300

    Update project with assignments instructions

--- src/index.js
+++ src/index.js
@@ -1,11 +1,29 @@
 import _ from 'lodash';
+import printMe from './print.js';
+import './style.css';
+import Icon from './icon.png'
+import Data from './data.xml';
+import Notes from './data.csv';
+
+
 
 function component() {
   const element = document.createElement('div');
-
-  // Lodash, now imported by this script
-
+  const btn = document.createElement('button');
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
+  btn.innerHTML = 'Click me and check the console!';
+  // Add the image to our existing div.
+  const myIcon = new Image();
+  myIcon.src = Icon;
+
+  element.appendChild(myIcon);
+  btn.onclick = printMe;
+
+  element.appendChild(btn);
+
+  console.log(Data);
+  console.log(Notes);
 
   return element;
 }
